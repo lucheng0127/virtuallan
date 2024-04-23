12b976e35c7fe4671fb40d453276ce72321d792b
lucheng <lucheng0127@outlook.com>
2024-04-23 17:04:41 +0800

    Auth client first before switch io

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -5,3 +5,7 @@ import "strings"
 func IsIPConflictErr(err error) bool {
 	return strings.Contains(err.Error(), "used by other endpoint")
 }
+
+func IsUnauthedErr(err error) bool {
+	return strings.Contains(err.Error(), "unauthed client")
+}
